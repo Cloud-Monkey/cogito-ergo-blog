c51cafcc86e8366914e2db23b48b249d9c5157da
Michael Brown <ironmonkeydaveb@yahoo.co.uk>
2024-09-06 11:37:47 +0000

    edit secret key and allowed hosts

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['8000-cloudmonkey-cogitoergob-hdkv9fqsyhc.ws.codeinstitute-ide.net', '.herokuapp.com']
+ALLOWED_HOSTS = ['8000-cloudmonkey-cogitoergob-hdkv9fqsyhc.ws.codeinstitute-ide.net', '.herokuapp.com', '8000-cloudmonkey-cogitoergob-m2g3xdyjiaz.ws.codeinstitute-ide.net']
 
 # Application definition
 
