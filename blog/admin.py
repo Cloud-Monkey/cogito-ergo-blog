36c0ee8b55ce4fb60311f4cca10f448a3ee9372b
Michael Brown <ironmonkeydaveb@yahoo.co.uk>
2024-09-10 08:47:04 +0000

    add: summernote path

--- blog/admin.py
+++ blog/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
+@admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
 
     list_display = ('title', 'slug', 'status')
@@ -10,6 +11,6 @@ class PostAdmin(SummernoteModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('content',)
 
+
 # Register your models here.
-admin.site.register(Post)
 admin.site.register(Comment)
